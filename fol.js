5f74a83408a90883d213ce037bf0ffc5fabc2445
sai <saishinde.model@gmail.com>
2023-06-07 18:13:36 +0530

    changes

---
+++ fol.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+
+const bodyparser=require('body-parser');
+app.arguments(bodyparser.urlencoded({extended:true}));
+
+app.listen(4000,function(){
+    console.log("new file started.. .. .. .. ..");
+});
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/fol.html");
+});
+
+app.post("/",function(req,res){
+    res.send("done");
+});
